2a3c8c18c7cf803b073035c024909fca33887920
Jaye Popplewell <jlpopp01@gmail.com>
2019-05-13 19:27:48 -0400

    Inital commit

---
+++ PracticeInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PracticeInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = "painter";
+
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Who is your favorite " + thing + "? ");
+            string favoriteThing = Console.ReadLine();
+
+            Console.Write(" My name is " + name + " and my favorite " + thing + " is " + favoriteThing + ". ");
+        }
+    }
+}
